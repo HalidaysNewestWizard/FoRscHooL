def4650298df9a562107da543215d7e04a715cbb
HalidaysNewestWizard <garthcarey0862@gmail.com>
2023-10-17 10:56:58 -0600

    changed to a formula and def

--- temp.py
+++ temp.py
@@ -1,4 +1,9 @@
-tempC=25
+def cel_to_Far():
+    temp = float(input("Enter temperature in Celsius : "))
+    tempertureF=(temp*9/5)+32
+    print(tempertureF)
+cel_to_Far()
+'''tempC=25
 tempertureF=(tempC*9/5)+32
 
-print(f"The temperature  in Fahrenheit is {tempertureF} degrees and the temperature in Celsius is {tempC}")
+print(f"The temperature  in Fahrenheit is {tempertureF} degrees and the temperature in Celsius is {tempC}")'''
